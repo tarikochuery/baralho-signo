a02d78b64f1b14608e286500611c0a38928e5f88
tarikochuery <ttarikechuery@gmail.com>
2021-06-18 22:36:31 -0300

    turn card developing

--- src/componentes/Card_signo/index.js
+++ src/componentes/Card_signo/index.js
@@ -3,9 +3,15 @@ import './style.css';
 
 const Card_signo = (props) => (
     <div className='card-container'>
-        <p>{props.nome}</p>
-        <img alt={props.nome} src={props.imagem} />
-        <p className='data'>{props.dataInicio} - {props.dataFim}</p>
+        <div className='card-container-front'>
+            <p>{props.nome}</p>
+            <img alt={props.nome} src={props.imagem} />
+            <p className='data'>{props.dataInicio} - {props.dataFim}</p>
+        </div>
+        <div className='card-container-back'>
+            <p>{props.name}</p>
+            <p>Aqui abaixo temos a descrição do nosso signo</p>
+        </div>
     </div>
 )
 
